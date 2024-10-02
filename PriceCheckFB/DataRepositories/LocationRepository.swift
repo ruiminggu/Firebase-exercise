871738c77be3dcc40c3014c5e65560ea91a9c781
ruiminggu <91765134+ruiminggu@users.noreply.github.com>
2024-10-01 22:38:45 -0400

    finish

--- PriceCheckFB/DataRepositories/LocationRepository.swift
+++ PriceCheckFB/DataRepositories/LocationRepository.swift
@@ -6,26 +6,35 @@
 
 import Combine
 // import Firebase modules here
+import Firebase
+import FirebaseFirestore
 
 
 
 
 class LocationRepository: ObservableObject {
-  // Set up properties here
-  
-  
-  
+  private let path = "location_scans"
+  private let store = Firestore.firestore()
+
+  @Published var locations: [Location] = []
+  private var cancellables: Set<AnyCancellable> = []
+
   init() {
-    get()
+      get()
   }
 
   func get() {
-    // Complete this function
-    
-    
-    
-    
-    
+      store.collection(path)
+          .addSnapshotListener { querySnapshot, error in
+              if let error = error {
+                  print("Error getting locations: \(error.localizedDescription)")
+                  return
+              }
 
+              self.locations = querySnapshot?.documents.compactMap { 
+                document in
+                try? document.data(as: Location.self)
+              } ?? []
+          }
   }
 }
